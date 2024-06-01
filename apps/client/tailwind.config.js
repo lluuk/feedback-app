eae69897a4ba89b39d58e8f19782b1ec6a72c015
lluuk <luk.zapolski@gmail.com>
2024-06-01 15:03:17 +0200

    chore: extend tailwind config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./**/*.vue'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: '#34D399',
+        gray: '#EAF0F6',
+        lightGray: '#F8FAFC',
+        textBlack: '#1E293B',
+        textGray: '#64748B',
+        borderGray: '#E2E8F0',
+      },
+      borderRadius: {
+        DEFAULT: '3px',
+      },
+      fontFamily: {
+        sans: ['"Inter var", sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
