3e280675f32ba5073f343adf886d58d3003d2771
lluuk <luk.zapolski@gmail.com>
2024-06-01 15:03:29 +0200

    feat: add BaseButton component

---
+++ apps/client/src/components/BaseButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+type ButtonVariant = 'primary' | 'secondary';
+
+interface Props {
+  variant: ButtonVariant;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: 'primary',
+});
+
+const buttonClasses = computed(() =>
+  props.variant === 'primary' ? 'bg-green text-white' : 'bg-gray text-black',
+);
+</script>
+
+<template>
+  <button
+    class="h-auto w-auto rounded text-sm px-[18px] py-[10px]"
+    :class="buttonClasses"
+  >
+    <slot />
+  </button>
+</template>
