da7b5111b603e2fe587eed30987f85a2ecdc0906
lluuk <luk.zapolski@gmail.com>
2024-06-03 01:50:37 +0200

    feat: update feedback item to handle feedback details

--- apps/client/src/components/FeedbackItem.vue
+++ apps/client/src/components/FeedbackItem.vue
@@ -7,10 +7,12 @@ import SuggestionIcon from '@/assets/images/icons/suggestion.svg';
 interface Props {
   feedback: FeedbackWithId;
   isSelected?: boolean;
+  variant: 'short' | 'full';
 }
 
 const props = withDefaults(defineProps<Props>(), {
   isSelected: false,
+  variant: 'short',
 });
 
 const formattedTimeAgo = useTimeAgo(props.feedback.createdAt as Date);
@@ -18,6 +20,7 @@ const formattedTimeAgo = useTimeAgo(props.feedback.createdAt as Date);
 
 <template>
   <div
+    v-if="variant === 'short'"
     class="flex p-2 justify-between gap-x-2 rounded-md cursor-pointer"
     :class="isSelected ? 'bg-light-gray' : 'bg-white'"
   >
@@ -25,12 +28,31 @@ const formattedTimeAgo = useTimeAgo(props.feedback.createdAt as Date);
       <BugIcon v-if="feedback.type === 'bug'" />
       <SuggestionIcon v-else />
       <div class="flex flex-col gap-y-2">
-        <div class="text-black">{{ feedback.title }}</div>
-        <div class="text-defaul-text-gray text-xs">{{ feedback.name }}</div>
+        <div class="text-black font-medium">{{ feedback.title }}</div>
+        <div class="text-defaul-text-gray text-xs font-medium">
+          {{ feedback.name }}
+        </div>
       </div>
     </div>
-    <div class="text-defaul-text-gray text-xs self-end">
+    <div class="text-defaul-text-gray text-xs self-end font-medium">
       {{ formattedTimeAgo }}
+    </div>
+  </div>
+  <div v-else class="flex gap-x-2">
+    <div>
+      <BugIcon v-if="feedback.type === 'bug'" />
+      <SuggestionIcon v-else />
+    </div>
+    <div class="flex flex-col gap-y-2">
+      <div class="text-defaul-text-gray text-xs font-medium">
+        {{ formattedTimeAgo }}
+      </div>
+      <div class="text-black text-2xl font-medium">{{ feedback.title }}</div>
+      <div class="text-defaul-text-gray text-xs font-medium">
+        {{ feedback.name }} ({{ feedback.email }})
+      </div>
+
+      <p v-html="feedback.message" />
     </div>
   </div>
 </template>
