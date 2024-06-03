b4d9ddd1911aade92f41862ecbfd591d88aceb45
lluuk <luk.zapolski@gmail.com>
2024-06-03 13:58:34 +0200

    fix: circular dependencies

--- packages/validation/src/main.ts
+++ packages/validation/src/main.ts
@@ -1,10 +1,9 @@
 import { z } from 'zod';
-import { FeedbackTypeEnum } from 'types';
 
 export const feedbackSchema = z.object({
   name: z.string().min(1),
   email: z.string().email().min(1),
-  type: z.enum([FeedbackTypeEnum.bug, FeedbackTypeEnum.suggestion]),
+  type: z.enum(['bug', 'suggestion']),
   title: z.string().min(1),
   message: z.string().min(1),
   createdAt: z.date().optional(),
